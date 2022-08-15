1568d4662b381484060a76d14874da3b41cb6947
whatihope <2386505140@qq.com>
2022-08-15 20:37:04 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,22 @@
+package com.atguigu.git;
+
+/**
+ * @author huangyarui
+ * @create 2022-08-15-8:01 PM
+ */
+public class GitTest {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+
+
+
+
+
+
+
+
+}
